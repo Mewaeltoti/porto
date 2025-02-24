8f8eb3cf0fe2846e38d423f7172a6042ea888898
Mewaeltoti <mewaeltoti@gmail.com>
2025-02-24 16:38:33 +0300

    ini

---
+++ src/info/Info.js
@@ -0,0 +1,108 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = [" rgb(240, 227, 224), rgb(70, 155, 247)"];
+
+
+
+
+
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+/* 
+This variable will change the layout of the website from multipage to single, scrollable page
+*/
+export let singlePage = false;
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Mewael",
+    lastName: "Werede",
+    initials: "<--------- >", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Full Stack Developer",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+       
+        {
+            emoji: '🌎',
+            text: 'based in the Ethiopia '
+        },
+        {
+            emoji: "💼",
+            text: "Freelancer"
+        },
+        {
+            emoji: "📧",
+            text: "mewaeltoti@@gmail.com"
+        }
+    ],
+    socials: [
+             
+        {
+            link: "https://github.com/Mewaeltoti",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        
+       
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Hello! I'm Mewael. I'm a Software engineer . I studied Software engineer at Microlink EThiopia, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
+            exposedTo: ['nodejs', 'adobe photoshop', 'adobe illustrator']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        }
+        
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Ethiopian Banks Exchange Rates",
+            live: "https://ebanks-exchange.vercel.app", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/Mewaeltoti/Ebanks-Exchange", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Movies list",
+            live: "https://toti-movies.vercel.app/",
+            source: "https://github.com/Mewaeltoti/movies-app",
+            image: mock2
+        },
+        
+        
+    ]
+}
